48cbf73647a601df8c96121642111c7cddb17888
Antonella Macri <112986244+antomacri@users.noreply.github.com>
2023-09-13 00:51:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,26 +16,58 @@ app.get('/', (req, res) => { res.status(200).end('¡Bienvenido a la API de ELECT
 app.get('/Electronica', async (req, res) => {
     const client = await connectToMongodb();
     if (!client) {
-        res.status(500).send('Error al conectarse a MongoDB')
+        res.status(500).send('Error al conectarse a MongoDB');
         return;
     }
-    const db = client.db('Electronica')
-    const Electronica = await db.collection('Electronica').find().toArray()
-    await disconnectToMongodb()
-    res.json(Electronica)
+    const db = client.db('Ingenias');
+    const Electronica = await db.collection('Electronica').find().toArray();
+    await disconnectToMongodb();
+    res.json(Electronica);
 });
-// ```
-app.get('/Electronica/:id', async (req, res) => {
-    const ElectronicaID = parseInt(req.params.id) || 0
+
+// :ID
+app.put('/Electronica/:id', async (req, res) => {
+    const ElectronicaID = parseInt(req.params.id) || 0;
+    const nuevoDatos = req.body; 
+
+    if (!nuevoDatos) {
+        res.status(400).send('Los nuevos datos son requeridos para la actualización.');
+        return;
+    }
+
     const client = await connectToMongodb();
     if (!client) {
-        res.status(500).send('Error al conectarse a MongoDB')
+        res.status(500).send('Error al conectarse a MongoDB');
         return;
     }
-    const db = client.db('Electronica')
-    const Electronica= await db.collection('Electronica').findOne({ id: frutaID})
-    await disconnectToMongodb()
-    !Electronica ? res.status(404).send('No encontre la fruta con el id '+ frutaID): res.json(fruta)
+
+    const db = client.db('Ingenias');
+    const filtro = { id: ElectronicaID };
+    const actualizacion = { $set: nuevoDatos };
+
+    const resultado = await db.collection('Electronica').updateOne(filtro, actualizacion);
+    await disconnectToMongodb();
+
+    if (resultado.modifiedCount === 0) {
+        res.status(404).send('No se encontró el producto electrónico con el ID ' + ElectronicaID);
+    } else {
+        res.json({ mensaje: 'Producto electrónico actualizado con éxito' });
+    }
+});
+
+ // Endponit por categoria 
+app.get('/Electronica/categoria/:categoria', async (req, res) => {
+    const categoriaElectronica = req.params.categoria;
+    const client = await connectToMongodb();
+    if (!client) {
+        res.status(500).send('Error al conectarse a MongoDB');
+        return;
+    }
+    const regex = new RegExp(categoriaElectronica.toLowerCase(), 'i');
+    const db = client.db('Ingenias');
+    const Electronica = await db.collection('Electronica').find({ categoria: regex }).toArray();
+    await disconnectToMongodb();
+    Electronica.length === 0 ? res.status(404).send('No se encontraron productos electrónicos con el nombre ' + nombreElectronica) : res.json(Electronica);
 });
 
 app.get('/Electronica/nombre/:nombre', async (req, res) => {
@@ -45,27 +77,48 @@ app.get('/Electronica/nombre/:nombre', async (req, res) => {
         res.status(500).send('Error al conectarse a MongoDB')
         return;
     }
-    const regex = new RegExp(nombreFruta.toLowerCase(), 'i');
-    const db = client.db('Electronica')
+    const regex = new RegExp(nombreElectronica.toLowerCase(), 'i');
+    const db = client.db('Ingenias')
     const Electronica = await db.collection('Electronica').find({ nombre: regex}).toArray()
     await disconnectToMongodb()
-    frutas.length == 0 ? res.status(404).send('No encontre la fruta con el nombre '+ nombreFruta): res.json(frutas)
+    Electronica.length == 0 ? res.status(404).send('No encontre del producto con el nombre '+ nombreElectronica): res.json(Electronica)
 })
 
-app.get('/Electronica /precio/:precio', async (req, res) => {
-    const precioFruta = parseInt(req.params.precio) || 0
+app.get('/Electronica/precio/:precio', async (req, res) => {
+    const precioElectronica = parseInt(req.params.precio) || 0
     const client = await connectToMongodb();
     if (!client) {
         res.status(500).send('Error al conectarse a MongoDB')
         return;
     }
-    const db = client.db('Electronica') 
+    const db = client.db('Ingenias') 
     // gte: mayor o igual a
-    const Electronica = await db.collection('Electronica').find({ importe: { $gte: precioFruta } }).toArray()
+    const Electronica = await db.collection('Electronica').find({ importe: { $gte: precioElectronica } }).toArray()
     await disconnectToMongodb()
-    frutas.length == 0 ? res.status(404).send('No encontre la fruta con el precio '+ precioFruta): res.json(frutas)
+    lectronica.length === 0 ? res.status(404).send('No se encontraron productos electrónicos con precio mayor o igual a ' + precioElectronica) : res.json(Electronica);
 
-})
+}) 
+//Agregar datos
+app.post('/Electronica', async (req, res) => {
+    const nuevoElectronico = req.body; // Suponiendo que los datos a agregar están en el cuerpo de la solicitud.
+    
+    if (!nuevoElectronico) {
+        res.status(400).send('Los datos del producto electrónico son requeridos.');
+        return;
+    }
+
+    const client = await connectToMongodb();
+    if (!client) {
+        res.status(500).send('Error al conectarse a MongoDB');
+        return;
+    }
+    
+    const db = client.db('Ingenias');
+    const resultado = await db.collection('Electronica').insertOne(nuevoElectronico);
+    await disconnectToMongodb();
+
+    res.json(resultado.ops[0]); // Devolver el objeto insertado con su nuevo ID.
+});
 
 app.get("*", (req, res) => {
   res.json({
@@ -75,4 +128,4 @@ app.get("*", (req, res) => {
 });
 
 //Inicia el servidor
-app.listen(PORT, () => console.log(`API de frutas escuchando en http://localhost:${PORT}`) );
+app.listen(PORT, () => console.log(`API de Electronica escuchando en http://localhost:${PORT}`) );
